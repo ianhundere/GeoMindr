39185284e209705eaa9581b959f2eb2849b79e2e
Ian Hundere <ianfundere@gmail.com>
2018-11-14 11:33:13 -0500

    adds schema

---
+++ schema.sql
@@ -0,0 +1,38 @@
+
+-- USERs
+-- username
+-- name
+-- phone #s
+-- avatar (maybe?)
+
+create table users
+(
+    id serial primary key,
+    name text,
+    username varchar(200) unique not null,
+    phone_number varchar(20) unique not null,
+);
+
+-- REMINDERS
+-- user_id
+-- reminder
+-- public/priv
+
+create table reminders
+(
+    id serial primary key,
+    reminder text,
+    is_public boolean,
+    user_id integer references users (id) on delete cascade
+);
+
+-- LOCATIONS
+-- latitude
+-- longitude
+
+create table locations
+(
+    id serial primary key,
+    latitude integer,
+    longitude integer,
+);
