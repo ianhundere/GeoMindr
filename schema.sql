872c49d2f9f8ff25b4e51c6895143af4af2f83b7
Ian Hundere <ianfundere@gmail.com>
2018-11-14 13:13:28 -0500

    adds location_id

--- schema.sql
+++ schema.sql
@@ -25,7 +25,8 @@ create table reminders
     id serial primary key,
     reminder text,
     is_public boolean,
-    user_id integer references users (id) on delete cascade
+    user_id integer references users (id) on delete cascade,
+    location_id integer references locations (id) on delete cascade
 );
 
 -- LOCATIONS
@@ -37,5 +38,4 @@ create table locations
     id serial primary key,
     latitude integer,
     longitude integer,
-    reminder_id integer references reminders (id) on delete cascade
 );
