2af36f639d17560e5751a186a4418cd97bc6d9f4
Stanley Chen <dannyshien@gmail.com>
2018-11-14 14:42:29 -0500

    create class reminder

---
+++ models/Reminder.js
@@ -0,0 +1,25 @@
+const db = require('./db');
+
+class Reminders {
+    constructor(id, reminder, is_public) {
+        this.id = id, 
+        this.reminder = reminder, 
+        this.is_public = is_public;
+    }
+
+    // === ===  CREATE  === ===
+    
+    
+    // === ===  RETRIEVE  === ===
+
+
+    // === ===  UPDATE  === ===
+
+
+    // === ===  DELETE  === ===
+
+
+
+
+}
+
