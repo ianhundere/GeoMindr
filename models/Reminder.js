250a68a902959b96578af3a240ee544c2e866726
Stanley Chen <dannyshien@gmail.com>
2018-11-14 15:34:30 -0500

    create query functions for create and retrieve

--- models/Reminder.js
+++ models/Reminder.js
@@ -8,9 +8,34 @@ class Reminders {
     }
 
     // === ===  CREATE  === ===
-    
+    static createReminder(reminder, is_public) {
+        return db.one(
+            `insert into reminders
+                (reminder, is_public)
+            values
+                ($1, $2)
+            returning id`, [reminder, is_public])
+            .then(result => {
+                const create = new Reminders(result.id, reminder, is_public);
+                return create;
+            });
+    }
     
     // === ===  RETRIEVE  === ===
+    static getById(id) {
+        return db.one(
+            `select * from reminders
+                where (id) = $1`, [id])
+            .then(result => {
+                const thisId = new Reminders(result);
+                return thisId; 
+            });
+    }
+
+    static getAll() {
+        return db.any(
+            `select * from reminders`);
+    }
 
 
     // === ===  UPDATE  === ===
