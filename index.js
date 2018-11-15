7f5afba75cbb2de0829b02acb8e421bb82a07dd0
Stanley Chen <dannyshien@gmail.com>
2018-11-15 15:45:21 -0500

    correct file path for views on index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const page = require('/views/page');
-const loginForm = require('/views/loginForm');
-const registrationForm = require('/views/registrationForm');
+const page = require('./views/page');
+const loginForm = require('./views/loginForm');
+const registrationForm = require('./views/registrationForm');
+
