8e5dfe5c9449f5b0ee3189859ce3b504f2a1ec05
Stanley Chen <dannyshien@gmail.com>
2018-11-15 15:34:42 -0500

    initial commit for frontend

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+
+const page = require('/views/page');
+const loginForm = require('/views/loginForm');
+const registrationForm = require('/views/registrationForm');
